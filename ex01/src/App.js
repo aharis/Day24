1a91d0b25abc73314aad6202ecc8e898891d9480
aharis <hare.arapovic@gmail.com>
2021-03-31 22:02:10 +0200

    dodao zadatak ex01

---
+++ ex01/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<button onClick={this.setMyStorage}>setMyStorage</button>
+			</div>
+		);
+	}
+	setMyStorsge() {
+		document.cookie = "Year = 2021";
+
+		localStorage.setItem("Paragon", "yes", "but Arena first");
+
+		sessionStorage.setItem("fronted", "React");
+	}
+}
+
+export default App;
